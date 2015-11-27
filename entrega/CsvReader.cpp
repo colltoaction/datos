3ee87cb67a4bb8c88b3569980a12ac8dd2f6148c
Martín Coll <martingonzalezcoll@gmail.com>
2015-11-27 18:21:31 -0300

    Predecimos!

--- entrega/CsvReader.cpp
+++ entrega/CsvReader.cpp
@@ -8,30 +8,31 @@
 CsvReader::CsvReader(std::istream &input)
   : input_(input) {
   std::string line;
-  if (std::getline(input_, line)) {
-    std::vector<std::string> header_tokens = split_line(line);
-    for (int i = 0; i < header_tokens.size(); ++i) {
-      headers_[i] = header_tokens[i];
-    }
-  } else {
+  if (!std::getline(input_, line)) {
     throw std::runtime_error("The input is empty");
+  }
+
+  // save headers and their index in the csv file
+  std::vector<std::string> header_tokens = split_line(line);
+  for (unsigned int i = 0; i < header_tokens.size(); ++i) {
+    headers_[i] = header_tokens[i];
   }
 }
 
 bool CsvReader::next(RowMap &row) {
   std::string line;
-  if (std::getline(input_, line)) {
-    // map tokens to keys
-    std::vector<std::string> tokens = split_line(line);
-    for (std::map<int, std::string>::iterator it = headers_.begin();
-         it != headers_.end(); it++) {
-      row[it->second] = tokens[it->first];
-    }
-
-    return true;
-  } else {
+  if (!std::getline(input_, line)) {
     return false;
   }
+
+  // map tokens to keys
+  std::vector<std::string> tokens = split_line(line);
+  for (std::map<int, std::string>::iterator it = headers_.begin();
+       it != headers_.end(); it++) {
+    row[it->second] = tokens[it->first];
+  }
+
+  return true;
 }
 
 std::vector<std::string> CsvReader::split_line(std::string &line) {
@@ -39,9 +40,9 @@ std::vector<std::string> CsvReader::split_line(std::string &line) {
 
   std::stringstream lineStream(line);
   std::string cell;
-
   while (std::getline(lineStream, cell, ',')) {
     result.push_back(cell);
   }
+
   return result;
 }
--- entrega/main.cpp
+++ entrega/main.cpp
@@ -4,31 +4,68 @@
 #include "CsvReader.h"
 
 
-int main() {
-  std::map<std::string, int> possible;
-  std::map<std::string, std::map<std::string, int>> favorable;
+std::string get_row_key(RowMap &row) {
+  return row["Zipcode"] += row["Daylight"] += row["CrimeAsociationDual"];
+}
 
-  std::ifstream train("/home/martin/Documents/repos/datos/entrega/trainFilteredSinBinarizarV3-columnas.csv");
+void train(std::map<std::string, int> &possible,
+           std::map<std::string, std::map<std::string, int>> &favorable,
+           std::ifstream &train) {
   CsvReader reader(train);
 
   RowMap row;
   while (reader.next(row)) {
-    std::string row_key = row["Zipcode"] += row["Daylight"] += row["CrimeAsociationDual"];
+    std::string row_key = get_row_key(row);
     possible[row_key] += 1;
     favorable[row_key][row["Category"]] += 1;
   }
+}
 
-  for (std::map<std::string, int>::iterator it = possible.begin();
-       it != possible.end(); it++) {
-    std::cout << "Key: " << it->first << std::endl
-              << "\tTotal: " << it->second << std::endl;
-    for (std::map<std::string, int>::iterator it2 = favorable[it->first].begin();
-         it2 != favorable[it->first].end(); it2++) {
-      std::cout << "\t" << it2->first << ": " << it2->second << std::endl;
-    }
-
-    std::cout << std::endl;
+void predict(std::map<std::string, int> &possible,
+             std::map<std::string, std::map<std::string, int>> &favorable,
+             std::ifstream &test,
+             std::ofstream &predicted) {
+  std::array<std::string, 39> categories = {"ARSON", "ASSAULT", "BAD CHECKS", "BRIBERY",
+                                            "BURGLARY", "DISORDERLY CONDUCT", "DRIVING UNDER THE INFLUENCE",
+                                            "DRUG/NARCOTIC", "DRUNKENNESS", "EMBEZZLEMENT", "EXTORTION",
+                                            "FAMILY OFFENSES", "FORGERY/COUNTERFEITING", "FRAUD",
+                                            "GAMBLING", "KIDNAPPING", "LARCENY/THEFT", "LIQUOR LAWS",
+                                            "LOITERING", "MISSING PERSON", "NON-CRIMINAL", "OTHER OFFENSES",
+                                            "PORNOGRAPHY/OBSCENE MAT", "PROSTITUTION",
+                                            "RECOVERED VEHICLE", "ROBBERY", "RUNAWAY", "SECONDARY CODES",
+                                            "SEX OFFENSES FORCIBLE", "SEX OFFENSES NON FORCIBLE",
+                                            "STOLEN PROPERTY", "SUICIDE", "SUSPICIOUS OCC", "TREA",
+                                            "TRESPASS", "VANDALISM", "VEHICLE THEFT", "WARRANTS", "WEAPON LAWS"};
+  // header
+  predicted << "Id";
+  for (size_t i = 0; i < categories.size(); ++i) {
+    predicted << "," << categories[i];
   }
+  predicted << std::endl;
+
+  CsvReader reader(test);
+
+  RowMap row;
+  while (reader.next(row)) {
+    predicted << row["Id"];
+    std::string row_key = get_row_key(row);
+    for (size_t i = 0; i < categories.size(); ++i) {
+      predicted << "," << (static_cast<long double>(favorable[row_key][categories[i]]) /
+                           possible[row_key]);
+    }
+    predicted << std::endl;
+  }
+}
+
+int main() {
+  std::map<std::string, int> possible;
+  std::map<std::string, std::map<std::string, int>> favorable;
+  std::ifstream train_f("/home/martin/Documents/repos/datos/entrega/trainFilteredSinBinarizarV3-columnas.csv");
+  std::ifstream test("/home/martin/Documents/repos/datos/entrega/testFilteredSinBinarizarV3-columnas.csv");
+  std::ofstream predicted("/media/martin/MARTIN/predicted.csv");
+
+  train(possible, favorable, train_f);
+  predict(possible, favorable, test, predicted);
 
   return 0;
 }
