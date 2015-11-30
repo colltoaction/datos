2a78d145507d391edd148b70f3c90f78b586d9f6
Martín Coll <martingonzalezcoll@gmail.com>
2015-11-30 02:20:07 -0300

    Muevo el header a un archivo y tomo el tiempo de ejecución.

--- entrega/submission.sh
+++ entrega/submission.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 ./compile.sh
-build/entrega /home/martin/Documents/repos/datos/entrega/trainFilteredSinBinarizarV3-columnas.csv \
-              /home/martin/Documents/repos/datos/entrega/testFilteredSinBinarizarV3-columnas.csv \
-    | 7zr a -si /media/martin/MARTIN/predicted.csv.7z
+cat header.txt
+echo
+echo "Presione una tecla para comenzar"
+read -n 1
+time build/entrega /home/martin/Documents/repos/datos/entrega/trainFilteredSinBinarizarV3-columnas.csv \
+                   /home/martin/Documents/repos/datos/entrega/testFilteredSinBinarizarV3-columnas.csv \
+    | 7zr a -si /home/martin/Documents/repos/datos/entrega/predicted.csv.7z
